772ff9aa7e6b58de6fdcb18477bd474067e2ac1c
Grobcar <davidgrobascarro@gmail.com>
2024-11-25 15:51:52 +0000

    Update Who.tsx

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -23,7 +23,7 @@ const Who = () => {
                 <img
                   src="/images/vet.webp"
                   alt="Equipo veterinario en acción"
-                  className="w-full h-auto max-w-full object-cover" 
+                  className="w-full max-w-full h-auto object-contain"
                   loading="lazy"
                 />
               </div>
